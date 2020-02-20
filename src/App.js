bd8c96593c101b2b8df53d6d40f265d82d3cbbdd
Samuel Grosenick <srgrosenick@gmail.com>
2020-02-19 21:03:41 -0900

    added map and building dataset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
+import { Map, Marker, Polygon, Popup, TileLayer } from 'react-leaflet';
 import logo from './logo.svg';
+import * as uwBuildings from "./data/UW_Mad_Buildings.json";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map center={[43.073051, -89.401230]} zoom={16}>
+      <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      {uwBuildings.features.map( building => (
+        <Polygon key={building.properties.BUILDINGFO} positions={building.geometry.coordinates} color="pink" stroke={2}/>
+      ))}
+      <Marker key="Marker" position={[43.073, -89.401]}></Marker>
+
+    </Map>
   );
 }
 
